e4468ff458c2c02c7738ecba85d612c7d12bd414
Wave Cantery Ambray <wacanam20@gmail.com>
2025-04-24 23:13:08 +0800

    feat: add Skeleton and Tooltip components
    - Introduced a new Skeleton component for loading states.
    - Added Tooltip component with TooltipProvider, TooltipTrigger, and TooltipContent for enhanced user interaction.
    
    refactor: update layout and structure of dashboard-related pages
    
    - Changed main layout of Dashboard, Detection, Encubations, and Encubation Details pages to use consistent padding.
    - Implemented a new DashboardLayout to encapsulate sidebar and breadcrumb navigation.
    
    feat: add useIsMobile hook
    
    - Created a custom hook to determine if the user is on a mobile device based on screen width.
    
    chore: update routing to use new DashboardLayout
    
    - Refactored routes to include DashboardLayout for better organization and structure.
    
    chore: update package dependencies
    
    - Added new Radix UI components for Avatar, Collapsible, Dialog, Dropdown Menu, Separator, and Tooltip.

---
+++ app/components/AppSidebar.tsx
@@ -0,0 +1,92 @@
+"use client"
+
+import * as React from "react"
+import {
+  AudioWaveform,
+  BookOpen,
+  Bot,
+  Command,
+  Cookie,
+  EggIcon,
+  Frame,
+  GalleryVerticalEnd,
+  LayoutDashboard,
+  Magnet,
+  Map,
+  PieChart,
+  Settings2,
+  SquareTerminal,
+} from "lucide-react"
+
+import { NavMain } from "~/components/MainNav"
+import { NavProjects } from "~/components/NavProjects"
+import { NavUser } from "~/components/NavUser"
+import { TeamSwitcher } from "~/components/TeamSwitcher"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "~/components/ui/sidebar"
+
+// This is sample data.
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m~example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  teams: [
+    {
+      name: "FICCO - Rice Duck Hatchery",
+      logo: Cookie,
+      plan: "Enterprise",
+    },
+    {
+      name: "ORO - Egg Blend",
+      logo: AudioWaveform,
+      plan: "Basic",
+    },
+    {
+      name: "Edward - Hatchback",
+      logo: Command,
+      plan: "Pro",
+    },
+  ],
+  navMain: [
+    {
+      title: "Dashboard",
+      url: "/dashboard",
+      icon: LayoutDashboard,
+      isActive: false
+    },
+    {
+      title: "Encubations",
+      url: "/encubations",
+      icon: EggIcon,
+    },
+    {
+      title: "Detection",
+      url: "/detection",
+      icon: Magnet,
+    }
+  ]
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <TeamSwitcher teams={data.teams} />
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  )
+}
--- app/dashboard/Dashboard.tsx
+++ app/dashboard/Dashboard.tsx
@@ -1,6 +1,6 @@
 export default function Dashboard() {
   return (
-    <main className="flex items-center justify-center pt-16 pb-4">
+    <main className="px-4">
       Dashboard Page
     </main>
   );
